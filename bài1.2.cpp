2cb27e3aa2351cabafcded4904fcd0781efa4351
1050080114 <125208717+1050080114@users.noreply.github.com>
2023-03-09 12:12:42 +0700

    Add files via upload

---
+++ bài1.2.cpp
@@ -0,0 +1,16 @@
+# include <stdio.h>
+int main()
+{
+     float a, b, c, d;
+    printf("\nNhap phan nguyen b: ");
+    scanf("%d", &a.b);
+
+    printf("\nNhap tu so c: ");
+    scanf("%d", &a.c);
+
+    printf("\nNhap mau so: ");
+    scanf("%d", &a.d);
+
+
+    printf("%d(%d/%d)", a.b , a.c , a.d);
+}
